4e2e453dbc0fa19c5d8a78e8226ed87476f8039c
Marco Marasca <marascamarco@gmail.com>
2019-02-15 16:50:16 -0800

    Sticky footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'hedgycloud'`, () => {
+  it(`should have as title 'Hedgy Cloud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('hedgycloud');
+    expect(app.title).toEqual('Hedgy Cloud');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to hedgycloud!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Hedgy Cloud!');
   });
 });
