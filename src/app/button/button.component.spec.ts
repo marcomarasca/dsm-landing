db33246c092faa030062e1f3245fb90aa277052c
Marco Marasca <marascamarco@gmail.com>
2019-02-19 15:29:58 -0800

    Added a few more tests

--- src/app/button/button.component.spec.ts
+++ src/app/button/button.component.spec.ts
@@ -28,4 +28,48 @@ describe('ButtonComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should use the input app`, () => {
+    expect(component.app.name).toEqual('Test App');
+    expect(component.app.icon).toEqual('test');
+    expect(component.app.route).toEqual('test');
+  });
+
+  it('should use the correct icon', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.fa-' + component.app.icon)).toBeTruthy();
+  });
+
+  it('should set the correct name', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelectorAll('span')[1].textContent).toContain(component.app.name);
+  });
+
+  it('should disable the button when the app is disabled', () => {
+    const fixtureDisabled = TestBed.createComponent(ButtonComponent);
+    const componentDisabled = fixtureDisabled.componentInstance;
+    componentDisabled.app = {
+      name: 'Test Disabled',
+      icon: 'test',
+      route: 'test',
+      disabled: true
+    };
+    fixtureDisabled.detectChanges();
+    const compiled = fixtureDisabled.debugElement.nativeElement;
+    expect(compiled.querySelector('.icon-disabled')).toBeTruthy();
+  });
+
+  it('should add the redirect icon when the route is redirect', () => {
+    const fixtureRedirect = TestBed.createComponent(ButtonComponent);
+    const componentRedirect = fixtureRedirect.componentInstance;
+    componentRedirect.app = {
+      name: 'Test Disabled',
+      icon: 'test',
+      route: 'redirect'
+    };
+    fixtureRedirect.detectChanges();
+    const compiled = fixtureRedirect.debugElement.nativeElement;
+    expect(compiled.querySelector('.icon-redirect')).toBeTruthy();
+  });
+
 });
