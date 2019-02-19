3ad8bc2f63580d76d460ed3710d6e3c6d6371c7f
Marco Marasca <marascamarco@gmail.com>
2019-02-18 18:52:05 -0800

    Stying improvements

--- src/app/application.ts
+++ src/app/application.ts
@@ -11,8 +11,8 @@ export interface IApplication {
 export const APP_LIST: IApplication[] = [
   { name: 'Live', icon: 'tv', route: '/live', disabled: true },
   { name: 'Photo', icon: 'images', route: 'redirect', url: environment.baseUrl + '/photo' },
-  { name: 'Files', icon: 'file-code', route: 'redirect', url: environment.baseUrl + '/file' },
   { name: 'Video', icon: 'video', route: 'redirect', url: environment.baseUrl + '/video' },
+  { name: 'Files', icon: 'file-code', route: 'redirect', url: environment.baseUrl + '/file' },
   { name: 'Download', icon: 'download', route: 'redirect', url: environment.baseUrl + '/download' },
   { name: 'Admin', icon: 'cogs', route: 'redirect', url: environment.baseUrl + ':5001' }
 ];
