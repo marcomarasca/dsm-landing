6680068b21d845327310b2abd65c9d333b1dfd7b
pmb <pm.broca@gmail.com>
2020-06-16 13:51:35 +0200

    feat: remove footer

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
@@ -10,7 +9,6 @@ describe('AppComponent', () => {
       imports: [ RouterTestingModule ],
       declarations: [
         HeaderComponent,
-        FooterComponent,
         AppComponent
       ],
     }).compileComponents();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { ButtonComponent } from './button/button.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -13,7 +12,6 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
     ButtonComponent,
     HomeComponent
   ],
