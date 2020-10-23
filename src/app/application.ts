4d3191e15ca06c9265a8b7e973ebbf28a43566fe
Marco Marasca <8505576+marcomarasca@users.noreply.github.com>
2020-10-23 00:51:29 -0700

    Adds Drive link

--- src/app/application.ts
+++ src/app/application.ts
@@ -9,10 +9,11 @@ export interface IApplication {
 }
 
 export const APP_LIST: IApplication[] = [
-  { name: 'Live', icon: 'tv', route: '/live', disabled: true },
+  { name: 'Admin', icon: 'cogs', route: 'redirect', url: environment.baseUrl + ':5001' },
+  { name: 'Drive', icon: 'hdd', route: 'redirect', url: environment.baseUrl + '/drive' },
   { name: 'Photo', icon: 'images', route: 'redirect', url: environment.baseUrl + '/photo' },
   { name: 'Video', icon: 'video', route: 'redirect', url: environment.baseUrl + '/video' },
   { name: 'Files', icon: 'file-code', route: 'redirect', url: environment.baseUrl + '/file' },
   { name: 'Download', icon: 'download', route: 'redirect', url: environment.baseUrl + '/download' },
-  { name: 'Admin', icon: 'cogs', route: 'redirect', url: environment.baseUrl + ':5001' }
+  { name: 'Live', icon: 'tv', route: '/live', disabled: true }
 ];
