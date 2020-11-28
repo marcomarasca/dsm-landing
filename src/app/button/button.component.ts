d1cff12139ce512cebaea4cbf4bff7a4b145fbc1
pmb <pm.broca@gmail.com>
2020-11-28 14:48:09 +0100

    replaces bazarr by photos

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -9,14 +9,14 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent implements OnInit {
-  static readonly local = '192.168.1.23';
+  static readonly local = '192.168.';
   @Input() app: IApplication;
 
   constructor() { }
 
   ngOnInit() {
     if (!this.app.url.includes('http')) {
-      if (window.location.hostname === ButtonComponent.local) {
+      if (window.location.hostname.startsWith(ButtonComponent.local)) {
         this.app.url = `http${this.app.isAdmin ? 's' : ''}://${ButtonComponent.local}:${this.app.localUrl}`;
       } else {
         this.app.url = `https://${this.app.url}${environment.baseUrl}`;
